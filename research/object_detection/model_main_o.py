77a954866e8fb3be3d5fe4c80a670271a2985b46
violynn <violynn@163.com>
2018-08-24 21:24:37 +0800

    Update model_main_o.py

--- research/object_detection/model_main_o.py
+++ research/object_detection/model_main_o.py
@@ -29,10 +29,15 @@ from object_detection import model_lib
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 flags = tf.app.flags
+
+ROOT_DIR = os.getcwd()
+TRAIN_DIR = os.path.join(ROOT_DIR, "training")
+CONFIG_DIR = os.path.join(ROOT_DIR, "pipeline.config")
+
 flags.DEFINE_string(
-    'model_dir', './training', 'Path to output model directory '
+    'model_dir', 'TRAIN_DIR', 'Path to output model directory '
     'where event and checkpoint files will be written.')
-flags.DEFINE_string('pipeline_config_path', './pipeline.config', 'Path to pipeline config file.')
+flags.DEFINE_string('pipeline_config_path', 'CONFIG_DIR', 'Path to pipeline config file.')
 flags.DEFINE_integer('num_train_steps', 20000, 'Number of train steps.')
 flags.DEFINE_integer('num_eval_steps', 5000, 'Number of train steps.')
 flags.DEFINE_string(
